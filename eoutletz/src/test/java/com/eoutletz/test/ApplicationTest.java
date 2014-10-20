557d21370b71b6c418198652fcd2aa683802d811
Raj Nagulapalle <rnagulapalle@paypal.com>
2014-10-19 17:24:03 -0700

    added mysql jar

--- eoutletz/src/test/java/com/eoutletz/test/ApplicationTest.java
+++ eoutletz/src/test/java/com/eoutletz/test/ApplicationTest.java
@@ -3,6 +3,11 @@ package com.eoutletz.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.eoutletz.Application;
 
